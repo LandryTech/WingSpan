f6ff893f07944cca4c5cfbd6045aef59adb8a4d4
Kaden Landry <landryk3@wit.edu>
2025-03-21 21:19:47 -0400

    Reset repository to empty state

--- src/Constants.java
+++
@@ -1,6 +0,0 @@
-public class Constants {
-    public static double Kp = 0.8;
-    public static double Ki = 0.002; // Won't overshoot at 0
-    public static double Kd = 0.1;
-
-}
--- src/Constants.py
+++
@@ -1,6 +0,0 @@
-from random import randint
-
-KP = 0.9  # Proportional gain for PID
-KI = 0.1  # Integral gain for PID
-KD = 0.05  # Derivative gain for PID
-PID_OUTPUT_LIMITS = (-5, 5)  # Limits for control surface adjustments (in degrees)
